77b5fb125793e588301f72db7afd9f8da41c08f3
chervonyi <yuri.ternopil@gmail.com>
2019-06-12 17:04:13 -0500

    Complete view of Card activity

--- app/src/main/java/chrapps/memo/activities/CardActivity.kt
+++ app/src/main/java/chrapps/memo/activities/CardActivity.kt
@@ -1,22 +1,15 @@
 package chrapps.memo.activities
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputType
 import android.view.inputmethod.EditorInfo
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
 import chrapps.memo.R
 import chrapps.memo.views.TaskEditView
-import android.content.Context.INPUT_METHOD_SERVICE
-import android.support.v4.content.ContextCompat.getSystemService
 import android.content.Context
-import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.view.View
+import android.widget.*
 
 
 class CardActivity : AppCompatActivity() {
@@ -25,6 +18,8 @@ class CardActivity : AppCompatActivity() {
     private lateinit var tasksContainer: LinearLayout
     private lateinit var titleView: TextView
     private lateinit var dummyElement: LinearLayout
+
+    private var selectedColorId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +37,33 @@ class CardActivity : AppCompatActivity() {
             false
         }
 
+        setOptionButtonViews()
         appendTask()
+    }
+
+
+    private fun setOptionButtonViews() {
+
+        // Some magic to get actual size of height with value of MATCH_PARENT
+        val sample = findViewById<ImageButton>(R.id.option_yellow)
+        sample.post {
+            val height = sample.height
+
+            val optionIds = arrayOf(
+                R.id.option_yellow,
+                R.id.option_red,
+                R.id.option_green,
+                R.id.option_cyan,
+                R.id.option_blue,
+                R.id.option_violet)
+
+            for (id in optionIds) {
+                // For each option ImageButton set width as the height (to make a circle)
+                val option = findViewById<ImageButton>(id)
+                option.layoutParams.width = height
+                option.requestLayout()
+            }
+        }
     }
 
     @SuppressLint("ResourceAsColor")
@@ -62,9 +83,22 @@ class CardActivity : AppCompatActivity() {
         tasksContainer.addView(row)
     }
 
+    fun selectOptionBackgroundColor(view: View) {
+        if (selectedColorId != 0) {
+            // Remove 'check' image from previous selection
+            findViewById<ImageButton>(selectedColorId).setImageResource(0)
+        }
+
+        selectedColorId = view.id
+
+        // St 'check' image for selected imageButton
+        (view as ImageButton).setImageResource(R.drawable.ic_check_white)
+    }
+
     fun removeFocus() {
         dummyElement.requestFocus()
 
+        // Hide keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(titleView.windowToken, 0)
     }
