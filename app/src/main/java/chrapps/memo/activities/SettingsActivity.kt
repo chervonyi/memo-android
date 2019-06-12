ab365f35bcbdff071bb30942147b09aa1a0dfe0a
chervonyi <yuri.ternopil@gmail.com>
2019-06-12 17:35:12 -0500

    Add Settings activity

---
+++ app/src/main/java/chrapps/memo/activities/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package chrapps.memo.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import chrapps.memo.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
